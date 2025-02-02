61cbff35477e2d6f2e59517d996da140984c4abf
Adam Robson <adamrayrobson@gmail.com>
2025-02-02 09:57:23 -0800

    add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,11 @@ export default function Home() {
   return (
     <div className="homepage can">
       <Background />
-      <Link href="/collections" className="navlink txt">collections</Link>
-      <Link href="/about" className="navlink txt">about</Link>
-      <Link href="/contact" className="navlink txt">contact</Link>
+      <div className="navlinks grid grid-cols-1 grid-rows-3">
+        <Link href="/collections" className="navlink txt">collections</Link>
+        <Link href="/about" className="navlink txt">about</Link>
+        <Link href="/contact" className="navlink txt">contact</Link>
+      </div>
     </div>
   );
 }
