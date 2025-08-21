4425a4c50ba6b7a449a13774213e12e45396edc1
Adam Robson <adamrayrobson@gmail.com>
2025-08-21 00:35:44 -0700

    add Word component and styles for animated word cycling in main layout

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ pnpm dev
 bun dev
 ```
 
+<WordCycle words={["music", "echoes", "silence", "cool", "noise", "vibration"]} />
+<WordCycle words={["quiet", "feeling", "riding", "relaxing", "partying", "goldfish"]} />
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `page.tsx`. The page auto-updates as you edit the file.
--- app/components/layout/main.tsx
+++ app/components/layout/main.tsx
@@ -1,10 +1,31 @@
 'use client';
-import CenterPiece from "../center-piece/center-piece";
+
+import Word from "@/components/word";
 
 export default function Main() {
   return (
-    <main className="flex-1 w-full">
-      <CenterPiece words={['happiness', 'sundown', 'echoes', 'silence', 'morning', 'dreaming', 'celebrating', 'lovers', 'night time']} hold={4} fade={1.5} />
+    <main className={`
+      flex-1 max-w-screen-sm w-full
+      mx-auto flex justify-center
+      items-center mt-60 opacity-80
+    `}>
+      <div className={`
+        text-center text-2xl 
+        md:text-3xl lg:text-4xl
+        tracking-widest leading-20
+      `}>
+        <p>
+          le fog
+        </p>
+        <p>is</p>
+        <p>
+          <Word words={["music", "noise", "tunes", "sounds", "murmurs", "nudges"]} intervalMs={8200} />
+        </p>
+        <p>for</p>
+        <p>
+          <Word words={["quiet", "feelings", "joy", "relaxing", "partying", "goldfish", "dreaming", "misfits", "love"]} intervalMs={4300} />
+        </p>
+      </div>
     </main>
   )
 }
